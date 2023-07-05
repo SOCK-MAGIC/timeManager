6320b9888027f9ced91642a44957c01d43123ad5
sock_magic <startxingcheng@gmail.com>
2023-07-05 21:11:53 +0800

    返回键逻辑修改

--- app/src/main/java/com/setruth/timemanager/MainActivity.kt
+++ app/src/main/java/com/setruth/timemanager/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.setruth.timemanager
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -10,6 +16,7 @@ import com.setruth.timemanager.ui.navigation.NavigationPage
 import com.setruth.timemanager.ui.screen.start.StartPageView
 import com.setruth.timemanager.ui.theme.TimeManagerTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.system.exitProcess
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -30,6 +37,18 @@ class MainActivity : ComponentActivity() {
                     composable(Route.Main.route) {
                         NavigationPage()
                     }
+                }
+            }
+            var firstTime by remember {
+                mutableStateOf(0L)
+            }
+            BackHandler {
+                val secondTime = System.currentTimeMillis()
+                if (secondTime - firstTime < 2000) {
+                    exitProcess(0)
+                } else {
+                    Toast.makeText(applicationContext, "再按一次返回键退出", Toast.LENGTH_SHORT).show()
+                    firstTime = System.currentTimeMillis()
                 }
             }
         }
